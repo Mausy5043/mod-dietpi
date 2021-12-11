1a2b9fdf222cb4a89b4f014de745ed30beb690be
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2021-12-11 09:03:37 +0100

    update for next stage

--- machines/rbian/Automation_Custom_PreScript.sh
+++ machines/rbian/Automation_Custom_PreScript.sh
@@ -11,48 +11,6 @@ mkdir -p /boot/.bin
   echo "***************************************"
   echo ""
 
-  echo "Repairing locale..."
-  # repair the locale to suppress errors during SSH-sessions like these:
-  # perl: warning: Setting locale failed.
-  # or
-  # -bash: warning: setlocale: LC_CTYPE: cannot change locale (UTF-8)
-  /usr/sbin/update-locale LC_ALL="en_US.UTF-8"
-  /usr/sbin/update-locale LANGUAGE="en_US:en"
-
-  echo ""
-  echo "Replacing user dietpi by user pi..."
-  # TODO: add user `pi` with sudo-rights and groups: "adm,users,video,dialout"
-  # add new user `pi`
-  # rename user
-  usermod -l pi dietpi
-  # reassign groups
-  groupmod -n pi dietpi
-  # rename home directory
-  back=$(pwd)
-  cd /home/dietpi
-    usermod -d /home/pi -m pi
-  cd "$back"
-
-  # add new user `pi` to sudoers
-  echo "pi ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/012_pi-nopasswd
-
-  # TODO: revoke root-access via SSH
-  echo ""
-  echo "Revoking root's SSH-access for security reasons..."
-  #sed -i "s/^PermitRootLogin/#&/" /etc/ssh/sshd_config
-
-  echo ""
-  echo "Updating /etc/hosts file..."
-  {
-    echo "###### Added by Automation_Custom_PreScript.sh"
-    echo "::1             localhost ip6-localhost ip6-loopback"
-    echo "fe00::0         ip6-localnet"
-    echo "ff00::0         ip6-mcastprefix"
-    echo "ff02::1         ip6-allnodes"
-    echo "ff02::2         ip6-allrouters"
-    echo "###### Added by Automation_Custom_PreScript.sh"
-  }>> /rootfs/etc/hosts
-
   # DEBUG: find out more about the state of the machine at this point
   dmesg -HkuPxe
 
--- machines/rbian/Automation_Custom_Script.sh
+++ machines/rbian/Automation_Custom_Script.sh
@@ -9,6 +9,43 @@
   date  +"%Y.%m.%d %H:%M:%S"
   echo ""
 
+  echo "Repairing locale..."
+  # repair the locale to suppress errors during SSH-sessions like these:
+  # perl: warning: Setting locale failed.
+  # or
+  # -bash: warning: setlocale: LC_CTYPE: cannot change locale (UTF-8)
+  /usr/sbin/update-locale LC_ALL="en_US.UTF-8"
+  /usr/sbin/update-locale LANGUAGE="en_US:en"
+
+  echo ""
+  echo "Replacing user dietpi by user pi..."
+  # TODO: add user `pi` with sudo-rights and groups: "adm,users,video,dialout"
+  # add new user `pi`
+  # rename user
+  userdel -Z -r dietpi
+  useradd -m -u 1000 -g 1000 -G adm,audio,dialout,sudo,gpio,systemd-journal,users,video pi
+
+  # add new user `pi` to sudoers
+  echo "pi ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/012_pi-nopasswd
+
+  # TODO: revoke root-access via SSH
+  echo ""
+  echo "Revoking root's SSH-access for security reasons..."
+  #sed -i "s/^PermitRootLogin/#&/" /etc/ssh/sshd_config
+
+  echo ""
+  echo "Updating /etc/hosts file..."
+  {
+    echo "###### Added by Automation_Custom_PreScript.sh"
+    echo "::1             localhost ip6-localhost ip6-loopback"
+    echo "fe00::0         ip6-localnet"
+    echo "ff00::0         ip6-mcastprefix"
+    echo "ff02::1         ip6-allnodes"
+    echo "ff02::2         ip6-allrouters"
+    echo "###### Added by Automation_Custom_PreScript.sh"
+  }>> /etc/hosts
+
+
   # TODO: review these cmdline settings:
   # TODO: cmdline="dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 noatime loglevel=6 cgroup_enable=memory elevator=noop fsck.repair=yes"
   # TODO: noatime` added to reduce wear on SD-card
