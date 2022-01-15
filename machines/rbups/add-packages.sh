de1a5f345503df14c7d40f74f5db5dc105c5fc8f
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-01-15 12:57:10 +0100

    add target location for clone

--- machines/rbups/add-packages.sh
+++ machines/rbups/add-packages.sh
@@ -8,6 +8,6 @@ install_package nut
 install_package graphviz
 
 echo "Installing UPSDIAGD package..."
-su -c "git clone https://gitlab.com/mausy5043-diagnostics/upsdiagd.git" pi
+su -c "git clone https://gitlab.com/mausy5043-diagnostics/upsdiagd.git /home/pi/upsdiagd" pi
 chmod -R 0755 "/home/pi/upsdiagd"
 su -c '/home/pi/upsdiagd/install.sh' pi
