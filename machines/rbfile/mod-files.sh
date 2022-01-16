8fae92b78aa28ca4667cfe09609e3759ffa1e099
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-01-16 10:47:41 +0100

    enable and start the remote journaling socket

---
+++ machines/rbfile/mod-files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ""
+echo -n "Modify /boot/config.txt "
+{
+  echo ""
+  echo "#>>> Added by Automation_Custom_Script.sh"
+  # echo "dtoverlay=lirc-rpi,gpio_in_pin=17,gpio_out_pin=22"
+  echo "dtparam=pwr_led_trigger=heartbeat"
+  echo "dtparam=act_led_trigger=heartbeat"
+  echo "#<<< Added by Automation_Custom_Script.sh"
+  echo ""
+}>> /boot/config.txt
+
+sudo systemctl enable systemd-journal-remote.socket
+sudo systemctl start systemd-journal-remote.socket
