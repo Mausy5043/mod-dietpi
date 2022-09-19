d6607591a7e214325ded76d60123a55863ad8c89
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-09-19 11:02:00 +0200

    use new method

--- machines/rbair/add-packages.sh
+++ machines/rbair/add-packages.sh
@@ -12,4 +12,4 @@ echo
 echo "Installing KIMNATY package..."
 su -c "git clone https://github.com/Mausy5043/kimnaty.git /home/pi/kimnaty" pi
 chmod -R 0755 "/home/pi/kimnaty"
-su -c "/home/pi/kimnaty/install.sh" pi
+su -c "/home/pi/kimnaty/kimnaty --install" pi
