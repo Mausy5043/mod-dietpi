35dc202b00a135abf0d5c42b1afcd6e5228d343f
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-11-06 08:51:14 +0100

    install bups too

--- machines/rbups/add-packages.sh
+++ machines/rbups/add-packages.sh
@@ -13,4 +13,10 @@ echo
 echo "Installing UPSDIAGD package..."
 su -c "git clone https://github.com/Mausy5043/upsdiagd.git /home/pi/upsdiagd" pi
 chmod -R 0755 "/home/pi/upsdiagd"
-su -c '/home/pi/upsdiagd/install.sh' pi
+# su -c '/home/pi/upsdiagd/install.sh' pi
+
+echo
+echo "Installing BUPS package..."
+su -c "git clone https://github.com/Mausy5043/bups.git /home/pi/bups" pi
+chmod -R 0755 "/home/pi/bups"
+su -c "/home/pi/bups/bups --install" pi
