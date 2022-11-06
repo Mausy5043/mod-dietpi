bde377eaa7479fb8af663ffcddbd291a162feb61
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-11-06 08:48:46 +0100

    uninstall bups too

--- machines/rbups/uninstall.sh
+++ machines/rbups/uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# stop aircon and remove services etc.
-pushd "/home/pi/upsdiagd"
-  ./uninstall.sh
-popd
+# stop upsdiagd and remove services etc.
+/home/pi/upsdiagd/uninstall.sh
+
+# stop bups and remove services etc.
+pushd "/home/pi/bups" || exit 0
+  ./bups --uninstall
+popd || exit 0
