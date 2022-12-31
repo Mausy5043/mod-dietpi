1250329044df6fe66556570e24005ffc8c431a47
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-12-31 09:01:15 +0100

    only execute if exists

--- machines/rbair/uninstall.sh
+++ machines/rbair/uninstall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # stop kimnaty and remove services etc.
-pushd "/home/pi/kimnaty" || exit 0
-  su -c "/home/pi/kimnaty/kimnaty --uninstall" pi
-  # remove rclone's local mountpoint
-  sudo rm -rv /srv/rmt
-popd || exit 0
+if [ -d /home/pi/kimnaty ]; then
+  pushd "/home/pi/kimnaty" || exit 0
+    su -c "/home/pi/kimnaty/kimnaty --uninstall" pi
+    # remove rclone's local mountpoint
+    sudo rm -rv /srv/rmt
+  popd || exit 0
+fi
--- machines/rbelec/uninstall.sh
+++ machines/rbelec/uninstall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # stop kimnaty and remove services etc.
-pushd "/home/pi/lektrix" || exit 0
-  su -c "/home/pi/lektrix/lektrix --uninstall" pi
-   # remove rclone's local mountpoint
-  sudo rm -rv /srv/rmt
+if [ -d /home/pi/lektrix ]; then
+  pushd "/home/pi/lektrix" || exit 0
+    su -c "/home/pi/lektrix/lektrix --uninstall" pi
+     # remove rclone's local mountpoint
+    sudo rm -rv /srv/rmt
 popd || exit 0
+fi
