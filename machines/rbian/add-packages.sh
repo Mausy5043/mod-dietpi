08ded2925b8d8efdf3ba8bad88670e5567a1b838
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-07-11 20:24:07 +0200

    reduced number of packages

--- machines/rbian/add-packages.sh
+++ machines/rbian/add-packages.sh
@@ -2,9 +2,9 @@
 
 echo
 echo "Installing additional packages..."
-install_apt_package build-essential
+# install_apt_package build-essential
 install_apt_package graphviz
-install_apt_package "tmux"
+install_apt_package tmux
 #install_apt_package libatlas-base-dev
 #install_apt_package libglib2.0-dev
 #install_apt_package libjpeg62
@@ -14,14 +14,14 @@ install_apt_package "tmux"
 #install_apt_package libxcb1
 install_apt_package man
 # install_apt_package nut
-install_apt_package picocom
-install_apt_package python3
-install_apt_package python3-dev
-install_apt_package python3-pip
-install_apt_package python3-scipy
-install_apt_package python3-serial
-install_apt_package sqlite3
-install_apt_package wavemon
+# install_apt_package picocom
+# install_apt_package python3
+# install_apt_package python3-dev
+# install_apt_package python3-pip
+# install_apt_package python3-scipy
+# install_apt_package python3-serial
+# install_apt_package sqlite3
+# install_apt_package wavemon
 
 # required for HDD-support
 install_apt exfat-fuse
@@ -29,22 +29,22 @@ install_apt exfat-utils
 install_apt ntfs-3g
 
 # required for hardware support (Bluetooth)
-install_apt_package pi-bluetooth
-install_apt_package bluetooth
-install_apt_package bluez
+# install_apt_package pi-bluetooth
+# install_apt_package bluetooth
+# install_apt_package bluez
 
-sudo addgroup --gid 112 bluetooth
-sudo usermod -aG bluetooth pi
-sudo rm /etc/modprobe.d/dietpi-disable_bluetooth.conf
-sudo sed -i /^[[:blank:]]*dtoverlay=disable-bt/d /boot/config.txt
+# sudo addgroup --gid 112 bluetooth
+# sudo usermod -aG bluetooth pi
+# sudo rm /etc/modprobe.d/dietpi-disable_bluetooth.conf
+# sudo sed -i /^[[:blank:]]*dtoverlay=disable-bt/d /boot/config.txt
 
 
 # required for hardware support (I2C & SPI)
-install_apt_package python3-rpi.gpio
-install_apt_package python3-smbus
-install_apt_package i2c-tools
-install_apt_package spi-tools
+# install_apt_package python3-rpi.gpio
+# install_apt_package python3-smbus
+# install_apt_package i2c-tools
+# install_apt_package spi-tools
 
-sudo addgroup --gid 114 spi
-sudo usermod -aG spi,i2c,plugdev pi
-sudo chown root:spi /dev/spidev0.*
+# sudo addgroup --gid 114 spi
+# sudo usermod -aG spi,i2c,plugdev pi
+# sudo chown root:spi /dev/spidev0.*
