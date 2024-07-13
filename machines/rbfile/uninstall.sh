6cdfdfde8bfaf214c7947029a8eaaef35a447fdd
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-07-13 14:44:16 +0200

    also remove mountpoint from fstab

--- machines/rbfile/uninstall.sh
+++ machines/rbfile/uninstall.sh
@@ -8,9 +8,11 @@ sleep 10
 if [ -d /srv/hdd ]; then
   echo "unmounting /mnt/dietpi_userdata"
   sudo umount /mnt/dietpi_userdata
+  sed -i '/ \/mnt\/dietpi_userdata /d' /etc/fstab
 fi
 
 # unmount USB-drive & remove mountpoint
+echo "unmounting HDD"
 findmnt -rno TARGET "/srv/hdd" | sudo xargs -rL1 umount
 sed -i '/ \/srv\/hdd /d' /etc/fstab
 rmdir -v /srv/hdd
