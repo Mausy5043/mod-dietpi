a91089e495a5a15172e81ee053239da4a500b126
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-07-14 09:10:28 +0200

    also remove from fstab

--- machines/rbfile/uninstall.sh
+++ machines/rbfile/uninstall.sh
@@ -10,6 +10,7 @@ echo "unmounting /home/qbittorrent/.config"
   sudo umount /home/qbittorrent/.config
 echo "unmounting /home/qbittorrent/.local"
   sudo umount /home/qbittorrent/.local
+sed -i '/qbittorrent/d' /etc/fstab
 if [ -d /srv/hdd ]; then
   echo "unmounting /mnt/dietpi_userdata"
   sudo umount /mnt/dietpi_userdata
